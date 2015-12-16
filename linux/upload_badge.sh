809cae4247bc148877413fd4aa0960d4f10e0da9
oscarkramer <okramer@radiantblue.com>
2015-12-16 15:07:54 -0500

    Debugging

--- linux/upload_badge.sh
+++ linux/upload_badge.sh
@@ -6,7 +6,7 @@ echo
 echo "upload_badge.sh: AGENT=<$AGENT>, STATUS=<$STATUS>"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $SCRIPT_DIR/../images
-CMD="scp -vp $STATUS.png okramer@omar.ossim.org:$AGENT_status.png"
+CMD="scp -vp $STATUS.png okramer@omar.ossim.org:${AGENT}_status.png"
 echo "command: $CMD"
 $CMD
 echo
