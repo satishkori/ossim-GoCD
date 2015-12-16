04092cf41ef3f4ff62980b11416769d0051a0460
oscarkramer <okramer@radiantblue.com>
2015-12-15 19:54:51 -0500

    Experimenting with badges

--- linux/upload_badge.sh
+++ linux/upload_badge.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $SCRIPT_DIR/../../ossim
 
 cp -f ../ossim-GoCD/images/$1.png current_status.png
-git commit -a -m "auto-update from GoCD" 
-git push
+#git commit -a -m "auto-update from GoCD" 
+#git push
 
 popd
