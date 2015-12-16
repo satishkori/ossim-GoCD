b8d1c17f405bde32d6dd7dee408f6f7217ee16df
oscarkramer <okramer@radiantblue.com>
2015-12-16 14:13:23 -0500

    fixed typo

--- linux/upload_badge.sh
+++ linux/upload_badge.sh
@@ -7,5 +7,5 @@ echo "upload_badge.sh: AGENT=<$AGENT>, STATUS=<$STATUS>"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $SCRIPT_DIR/../images
-scp -vp $STATUS.png okramer@omar.ossim.org:$AGENT_status.png
+scp -vp $STATUS.png okramer@omar.ossim.org:$AGENT_status.png:
 popd
