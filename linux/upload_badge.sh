2076d7ec81da8ad2b38bbd36f6eadcf288837a2d
oscarkramer <okramer@radiantblue.com>
2015-12-16 13:45:43 -0500

    Modified to use 3rd party host for badge image

--- linux/upload_badge.sh
+++ linux/upload_badge.sh
@@ -3,6 +3,8 @@
 AGENT=$1
 STATUS=$2
 
+echo "upload_badge.sh: AGENT=<$AGENT>, STATUS=<$STATUS>"
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $SCRIPT_DIR/../images
 scp -vp $STATUS.png okramer@omar.ossim.org:$AGENT_status.png
