94ed74f62bd2c6bc46c428b9aa66b6f42d833c0a
Garrett Potts <gpotts@radiantblue.com>
2016-01-08 08:56:21 -0500

    Added stage and branch into the image name.  Will need to adjust the readme accordingly.

--- scripts/upload_badge.sh
+++ scripts/upload_badge.sh
@@ -1,12 +1,14 @@
 #!/bin/bash 
 
 RESOURCE=$1
-STATUS=$2
+BRANCH=$2
+STATUS=$3
+STAGE=$4
 echo
 echo "upload_badge.sh: RESOURCE=<$RESOURCE>, STATUS=<$STATUS>"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $SCRIPT_DIR/../images
-CMD="scp -v $STATUS.png go@omar.ossim.org:${RESOURCE}_status.png"
+CMD="scp -v $STATUS.png go@omar.ossim.org:~/status/${RESOURCE}_${BRANCH}_${STAGE}_status.png"
 echo "command: $CMD"
 $CMD
 if [ $? != 0 ]; then
