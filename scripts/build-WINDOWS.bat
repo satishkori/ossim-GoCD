1ef3775473eaf41e4fe5392e92056c379619ce4e
Garrett Potts <gpotts@radiantblue.com>
2016-01-14 06:56:22 -0500

    Renamed the widnows build

---
+++ scripts/build-WINDOWS.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+set SCRIPT_DIR="%~dp0"
+
+call %SCRIPT_DIR%env-WINDOWS
+
+echo ************ Extracting dependencies ************
+cd %OSSIM_DEV_HOME%
+7z x -y ossim-deps-%OSSIM_DEPENCY_VERSION.zip
+
