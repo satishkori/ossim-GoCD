09483b8c66d5e0605b9409c1af3917cf36ce4964
Garrett Potts <gpotts@radiantblue.com>
2016-01-14 11:35:37 -0500

    removed extra backslash

--- scripts/build-WINDOWS.bat
+++ scripts/build-WINDOWS.bat
@@ -2,7 +2,7 @@
 rem setlocal enabledelayedexpansion
 set SCRIPT_DIR=%~dp0
 
-echo calling script %SCRIPT_DIR%\env-WINDOWS.bat
+echo calling script %SCRIPT_DIR%env-WINDOWS.bat
 call %SCRIPT_DIR%\env-WINDOWS.bat
 
 echo ************ Extracting dependencies: %OSSIM_DEV_HOME%\ossim-deps-%OSSIM_DEPENDENCY_VERSION%.zip ************
