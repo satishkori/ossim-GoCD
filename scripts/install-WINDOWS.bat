50554e4cb941e690146b8972828f3ade8835f32f
Garrett Potts <gpotts@radiantblue.com>
2016-01-17 16:38:33 -0500

    add installer for the windows side

---
+++ scripts/install-WINDOWS.bat
@@ -0,0 +1,22 @@
+@echo off
+set SCRIPT_DIR=%~dp0
+
+echo calling script %SCRIPT_DIR%env-WINDOWS.bat
+call %SCRIPT_DIR%\env-WINDOWS.bat
+
+echo ************ Extracting dependencies: %OSSIM_DEV_HOME%\ossim-deps-%OSSIM_DEPENDENCY_VERSION%.zip ************
+echo cd %OSSIM_DEV_HOME%
+cd %OSSIM_DEV_HOME%
+ 
+if not exist %OSSIM_DEV_HOME%build (
+  mkdir build
+)
+
+cd build 
+if ERRORLEVEL 1 exit 1
+jom install
+%OSSIM_DEV_HOME%ossim-GoCD\scripts\embed-manifests-WINDOWS.bat %OSSIM_INSTALL_PREFIX%\bin
+%OSSIM_DEV_HOME%ossim-GoCD\scripts\embed-manifests-WINDOWS.bat %OSSIM_INSTALL_PREFIX%\lib
+
+if ERRORLEVEL 1 exit 1
+
