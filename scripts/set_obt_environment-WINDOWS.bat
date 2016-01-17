ac3ad462d8fdc14296934a788afb881f4c957a93
Garrett Potts <gpotts@radiantblue.com>
2016-01-17 11:47:37 -0500

    Adding test phase for windows

---
+++ scripts/set_obt_environment-WINDOWS.bat
@@ -0,0 +1,60 @@
+
+::######################################################################################
+::#
+::# Script for setting OBT environment in GoCD. Required environment variables are:
+::#
+::#   OSSIM_DATA -- Local directory to contain elevation, imagery, and expected results
+::#
+::######################################################################################
+
+set SCRIPT_DIR=%~dp0
+cd %SCRIPT_DIR%\..\..
+set OSSIM_DEV_HOME=%CD%
+
+
+set OSSIM_INSTALL_DIR=%OSSIM_DEV_HOME%/install
+set PATH=%OSSIM_INSTALL_DIR%\bin:%PATH%
+set OSSIM_PREFS_FILE=%OSSIM_DEV_HOME%/ossim-GoCD/ossim-gocd.prefs
+set JAVA_CLASS_PATH=%OSSIM_INSTALL_DIR%/share/java
+
+echo "Checking for required environment variables..."
+
+::if [-z OSSIM_VERSION ]; then
+::   export OSSIM_VERSION=1.9.0
+::fi 
+
+::if [ -z $OSSIM_DATA ]; then
+::  echo "ERROR: The environment variable OSSIM_DATA is not defined. Aborting with error.";
+::  exit 1;
+::fi
+  
+::if [ ! -d $OSSIM_DATA ] || [ ! -w $OSSIM_DATA ]; then
+::  echo "ERROR: The directory <$OSSIM_DATA> does not exist or is not writable. Make sure the user has write permissions.";
+::  exit 1;
+::fi
+
+::if [ -z $OSSIM_BATCH_TEST_DATA ]; then
+::  export OSSIM_BATCH_TEST_DATA=$OSSIM_DATA/data
+::fi
+
+::if [ -z $OSSIM_BATCH_TEST_EXPECTED ]; then
+::  export OSSIM_BATCH_TEST_EXPECTED=$OSSIM_DATA/expected_results
+::fi
+
+::if [ -z $OSSIM_BATCH_TEST_RESULTS ]; then
+::  export OSSIM_BATCH_TEST_RESULTS=$OSSIM_DEV_HOME/results
+::fi
+
+::echo; echo "Test Environment:"
+::echo "  OSSIM_DEV_HOME            = $OSSIM_DEV_HOME"
+::echo "  OSSIM_DATA                = $OSSIM_DATA"
+::echo "  OSSIM_INSTALL_DIR         = $OSSIM_INSTALL_DIR"
+::echo "  OSSIM_PREFS_FILE          = $OSSIM_PREFS_FILE"
+::echo "  OSSIM_BATCH_TEST_DATA     = $OSSIM_BATCH_TEST_DATA"
+::echo "  OSSIM_BATCH_TEST_EXPECTED = $OSSIM_BATCH_TEST_EXPECTED"  
+::echo "  OSSIM_BATCH_TEST_RESULTS  = $OSSIM_BATCH_TEST_RESULTS"  
+::echo "  LD_LIBRARY_PATH           = $LD_LIBRARY_PATH"
+::echo "  PATH                      = $PATH"
+::echo
+
+
---
+++ scripts/sync_data-WINDOWS.bat
@@ -0,0 +1,122 @@
+::#####################################################################################
+::#
+::# Synchronizes the local GoCD agent machine with the OBT test data on repository
+::#
+::# Usage:  sync_data.sh <gocd_resource_name>
+::#
+::# Required ENV vars:
+::#
+::#   OSSIM_DATA_REPOSITORY -- local NFS mount point for data repository
+::#   OSSIM_DATA -- Local directory to contain elevation, imagery, and expected results
+::#
+::# Optional referenced  env var:
+::#
+::#   SKIP_EXPECTED_RESULTS_SYNC -- if defined, implies the data sync is for 
+::#      *generating* expected results, therefore prior expected results are not synced
+::#      from the repo (they are ignored).
+::#
+::#####################################################################################
+@echo off
+set GOCD_RESOURCE_NAME=%1
+
+echo; echo; 
+echo "################################################################################"
+echo "#  Running `basename "$0"` for resource <$1> out of <$PWD>"
+echo "################################################################################"
+
+set RSYNC_CMD="rsync -rlptvz"
+set SCRIPT_DIR=%~dp0
+
+# Set GoCD-specific environment:
+call $SCRIPT_DIR/set_obt_environment-WINDOWS.bat
+
+# Should already be there but create if not:
+if not exist "%OSSIM_DATA%\elevation" ( mkdir "%OSSIM_DATA%\elevation" )
+if not exist "%OSSIM_BATCH_TEST_DATA%\elevation" ( mkdir "%OSSIM_BATCH_TEST_DATA%\elevation" )
+if not exist "%OSSIM_BATCH_TEST_EXPECTED%\elevation" ( mkdir "%OSSIM_BATCH_TEST_EXPECTED%\elevation" )
+
+
+::echo; echo "STATUS: Checking access to data repository at <$OSSIM_DATA_REPOSITORY>...";
+::if [ -z $OSSIM_DATA_REPOSITORY ] || [ ! -d $OSSIM_DATA_REPOSITORY ] ; then
+::  echo "ERROR: Env var OSSIM_DATA_REPOSITORY must be defined and exist in order to syncronize against data repository.";
+::  echo; exit 1;
+::fi
+
+# rsync elevation data:
+echo; echo "STATUS: Syncing elevation data...";
+$RSYNC_CMD $OSSIM_DATA_REPOSITORY/elevation/dted/level0 $OSSIM_DATA/elevation/dted;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of elevation.";
+  echo; exit 1;
+fi
+
+# rsync nadcon data:
+echo; echo "STATUS: Syncing nadcon data...";
+$RSYNC_CMD $OSSIM_DATA_REPOSITORY/elevation/nadcon $OSSIM_DATA/elevation;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of nadcon grids.";
+  echo; exit 1;
+fi
+
+if [ ! -d $OSSIM_DATA/elevation/geoids ] ; then
+  echo; echo "STATUS: Creating missing geoids subdirectory";
+  mkdir $OSSIM_DATA/elevation/geoids; 
+  if [ $? != 0 ] ; then 
+    echo "ERROR: Failed creatiion of geoids directory at <$OSSIM_DATA/elevation/geoids>.";
+    echo; exit 1;
+  fi
+fi
+
+# rsync geoid 96 data:
+echo; echo "STATUS: Syncing geoid96 data...";
+$RSYNC_CMD $OSSIM_DATA_REPOSITORY/elevation/geoid96_little_endian/ $OSSIM_DATA/elevation/geoids/geoid96;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of geoid96 grids.";
+  echo; exit 1;
+fi
+
+# rsync geoid 99 data:
+echo; echo "STATUS: Syncing geoid99 data...";
+$RSYNC_CMD $OSSIM_DATA_REPOSITORY/elevation/geoid99_little_endian/ $OSSIM_DATA/elevation/geoids/geoid99;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of geoid99 grids.";
+  echo; exit 1;
+fi
+
+#rsync imagery
+echo; echo "STATUS: Syncing image data...";
+$RSYNC_CMD $OSSIM_DATA_REPOSITORY/test/data/public $OSSIM_BATCH_TEST_DATA;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of imagery.";
+  echo; exit 1;
+fi
+$RSYNC_CMD $OSSIM_DATA_REPOSITORY/test/data/geoeye1 $OSSIM_BATCH_TEST_DATA;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of imagery.";
+  echo; exit 1;
+fi
+$RSYNC_CMD $OSSIM_DATA_REPOSITORY/test/data/rbt $OSSIM_BATCH_TEST_DATA;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of imagery.";
+  echo; exit 1;
+fi
+  
+#rsync expected results (if exists)
+REPO_EXPECTED_RESULTS_DIR=$OSSIM_DATA_REPOSITORY/test/expected_results/$GOCD_RESOURCE_NAME
+echo; echo "STATUS: Checking for expected results in <$REPO_EXPECTED_RESULTS_DIR>...";
+echo "STATUS: SKIP_EXPECTED_RESULTS_SYNC = $SKIP_EXPECTED_RESULTS_SYNC";
+if [ -d $REPO_EXPECTED_RESULTS_DIR ] && [ -z $SKIP_EXPECTED_RESULTS_SYNC ]; then
+  echo; echo "STATUS: Syncing expected results from <$REPO_EXPECTED_RESULTS_DIR> to <$OSSIM_BATCH_TEST_EXPECTED>...";
+  $RSYNC_CMD $REPO_EXPECTED_RESULTS_DIR/ $OSSIM_BATCH_TEST_EXPECTED/;
+  if [ $? != 0 ] ; then 
+    echo "ERROR: Failed data repository rsync of expected results.";
+  echo; exit 1;
+  fi
+else
+  echo; echo "STATUS: Skipped sync of expected results" 
+fi
+
+echo 
+exit 0;
+
+
