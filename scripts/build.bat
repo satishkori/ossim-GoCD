0920ea02f88f2f9efa5bf0e030ab53ce5717ffb5
Garrett Potts <gpotts@radiantblue.com>
2016-01-18 10:33:25 -0500

    Fixed for new env windows

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,7 +1,7 @@
 @echo off
 set SCRIPT_DIR=%~dp0
 
-echo calling script %SCRIPT_DIR%env-WINDOWS.bat
+echo calling script %SCRIPT_DIR%env.bat
 call %SCRIPT_DIR%\env.bat
 
 echo ************ Extracting dependencies: %OSSIM_DEV_HOME%\ossim-deps-%OSSIM_DEPENDENCY_VERSION%.zip ************
