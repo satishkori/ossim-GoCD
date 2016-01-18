6f01b74e00cfa071ba01340a512521a615104d3b
Garrett Potts <gpotts@radiantblue.com>
2016-01-18 09:06:31 -0500

    Added function to fix the variables for rsync

--- scripts/sync_data.bat
+++ scripts/sync_data.bat
@@ -9,18 +9,21 @@ echo "##########################################################################
 set RSYNC_CMD=rsync -rlptvz
 set SCRIPT_DIR=%~dp0
 
-::Convert to cygdrive format.  The rsync on windows is a a cygwin port
-set DRIVE_ONLY=%OSSIM_DATA:~0,1%
-set FILE_NO_DRIVE=%OSSIM_DATA:~2,10000%
-set OSSIM_DATA_CYGDRIVE=/cygdrive/%DRIVE_ONLY%%FILE_NO_DRIVE%
+call:convertToRsync OSSIM_DATA_CYGDRIVE %OSSIM_DATA% 
+call:convertToRsync OSSIM_DATA_REPOSITORY_CYGDRIVE %OSSIM_DATA_REPOSITORY% 
 
-set DRIVE_ONLY=%OSSIM_DATA_REPOSITORY:~0,1%
-set FILE_NO_DRIVE=%OSSIM_DATA_REPOSITORY:~2,10000%
-set OSSIM_DATA_REPOSITORY_CYGDRIVE=/cygdrive/%DRIVE_ONLY%%FILE_NO_DRIVE%
+::Convert to cygdrive format.  The rsync on windows is a a cygwin port
+::set DRIVE_ONLY=%OSSIM_DATA:~0,1%
+::set FILE_NO_DRIVE=%OSSIM_DATA:~2,10000%
+::set OSSIM_DATA_CYGDRIVE=/cygdrive/%DRIVE_ONLY%%FILE_NO_DRIVE%
+
+::set DRIVE_ONLY=%OSSIM_DATA_REPOSITORY:~0,1%
+::set FILE_NO_DRIVE=%OSSIM_DATA_REPOSITORY:~2,10000%
+::set OSSIM_DATA_REPOSITORY_CYGDRIVE=/cygdrive/%DRIVE_ONLY%%FILE_NO_DRIVE%
 
 ::Let's make sure we are all forward slashes
-set OSSIM_DATA_REPOSITORY_CYGDRIVE=%OSSIM_DATA_REPOSITORY_CYGDRIVE:\=/%
-set OSSIM_DATA_CYGDRIVE=%OSSIM_DATA_CYGDRIVE:\=/%
+::set OSSIM_DATA_REPOSITORY_CYGDRIVE=%OSSIM_DATA_REPOSITORY_CYGDRIVE:\=/%
+::set OSSIM_DATA_CYGDRIVE=%OSSIM_DATA_CYGDRIVE:\=/%
 
 ::# Set GoCD-specific environment:
 call %SCRIPT_DIR%\set_obt_environment.bat
@@ -39,15 +42,15 @@ if not exist "%OSSIM_BATCH_TEST_EXPECTED%\elevation" ( mkdir "%OSSIM_BATCH_TEST_
 
 ::# rsync elevation data:
 echo; echo "STATUS: Syncing elevation data...";
-echo %RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%elevation/dted/level0 %OSSIM_DATA_CYGDRIVE%/elevation/dted;
+echo %RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%/elevation/dted/level0 %OSSIM_DATA_CYGDRIVE%/elevation/dted
 
-%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%elevation/dted/level0 %OSSIM_DATA_CYGDRIVE%/elevation/dted;
+%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%/elevation/dted/level0 %OSSIM_DATA_CYGDRIVE%/elevation/dted
 if ERRORLEVEL 1 exit 1
 
 ::# rsync nadcon data:
-echo; echo "STATUS: Syncing nadcon data...";
-echo %RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%elevation/nadcon %OSSIM_DATA_CYGDRIVE%/elevation;
-%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%elevation/nadcon %OSSIM_DATA_CYGDRIVE%/elevation;
+echo; echo "STATUS: Syncing nadcon data..."
+echo %RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%/elevation/nadcon %OSSIM_DATA_CYGDRIVE%/elevation
+%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%/elevation/nadcon %OSSIM_DATA_CYGDRIVE%/elevation
 if ERRORLEVEL 1 exit 1
 
 if not exist %OSSIM_DATA%\elevation\geoids (
@@ -55,25 +58,25 @@ if not exist %OSSIM_DATA%\elevation\geoids (
 )
 
 ::# rsync geoid 96 data:
-echo; echo "STATUS: Syncing geoid96 data...";
-echo %RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%elevation/geoid96_little_endian/ %OSSIM_DATA_CYGDRIVE%/elevation/geoids/geoid96;
-%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%elevation/geoid96_little_endian/ %OSSIM_DATA_CYGDRIVE%/elevation/geoids/geoid96;
+echo; echo "STATUS: Syncing geoid96 data..."
+echo %RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%/elevation/geoid96_little_endian/ %OSSIM_DATA_CYGDRIVE%/elevation/geoids/geoid96
+%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%/elevation/geoid96_little_endian/ %OSSIM_DATA_CYGDRIVE%/elevation/geoids/geoid96
 if ERRORLEVEL 1 exit 1
 
 ::# rsync geoid 99 data:
 echo; echo "STATUS: Syncing geoid99 data...";
-%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%elevation/geoid99_little_endian/ %OSSIM_DATA_CYGDRIVE%/elevation/geoids/geoid99;
+%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%/elevation/geoid99_little_endian/ %OSSIM_DATA_CYGDRIVE%/elevation/geoids/geoid99
 if ERRORLEVEL 1 exit 1
 
 ::#rsync imagery
 echo; echo "STATUS: Syncing image data...";
-%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%test/data/public %OSSIM_BATCH_TEST_DATA_CYGDRIVE%;
+%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%/test/data/public %OSSIM_BATCH_TEST_DATA_CYGDRIVE%
 if ERRORLEVEL 1 exit 1
 
-%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%test/data/geoeye1 %OSSIM_BATCH_TEST_DATA_CYGDRIVE%;
+%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%/test/data/geoeye1 %OSSIM_BATCH_TEST_DATA_CYGDRIVE%
 if ERRORLEVEL 1 exit 1
 
-%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%test/data/rbt %OSSIM_BATCH_TEST_DATA_CYGDRIVE%;
+%RSYNC_CMD% %OSSIM_DATA_REPOSITORY_CYGDRIVE%/test/data/rbt %OSSIM_BATCH_TEST_DATA_CYGDRIVE%
 if ERRORLEVEL 1 exit 1
   
 ::#rsync expected results (if exists)
@@ -94,4 +97,19 @@ echo "STATUS: SKIP_EXPECTED_RESULTS_SYNC = %SKIP_EXPECTED_RESULTS_SYNC%";
 ::echo 
 ::exit 0;
 
+
+goto :done
+
+
+:convertToRsync
+setlocal 
+set DRIVE_ONLY=%2:~0,1%
+set FILE_NO_DRIVE=%2:~2,10000%
+set tempVar = /cygdrive/%DRIVE_ONLY%%FILE_NO_DRIVE%
+set tempVar = %tempVar:\=/%
+if "%tempVar:~-1"=="/" (set tempVar=%tempVar:~0,-1% )
+endlocal  & SET %1=%tempVar%
+GOTO :EOF
+
+:done
 
