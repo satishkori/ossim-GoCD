596dc6efcfe5ab4ed5a3e7266662cd6025d38651
Garrett Potts <gpotts@radiantblue.com>
2016-01-19 09:31:22 -0500

    Will remove the find for plugins

--- scripts/test.bat
+++ scripts/test.bat
@@ -34,15 +34,15 @@ ossim-info --version | FINDSTR /C:%SUBSTRING% >nul & IF ERRORLEVEL 1 (
      echo Testing against wrong version
      exit 1
 )
-
 echo "STATUS: Passed ossim-info --config test.";
-echo; echo "STATUS: Running ossim-info --plugins test...";
-set SUBSTRING="ossim_kakadu_plugin ossim_png_plugin ossim_sqlite_plugin"
-echo "ossim-info --plugins | FINDSTR /i /r /C:%SUBSTRING% >nul & IF ERRORLEVEL 1"
-ossim-info --plugins | FINDSTR /C:%SUBSTRING% >nul & IF ERRORLEVEL 1 (
-     echo "ERROR: Plugins, kakadu, png, and sqlite are needed for base testing and were not found."
-     exit 1
-)
+
+::echo; echo "STATUS: Running ossim-info --plugins test...";
+::set SUBSTRING="ossim_kakadu_plugin ossim_png_plugin ossim_sqlite_plugin"
+::echo "ossim-info --plugins | FINDSTR /i /r /C:%SUBSTRING% >nul & IF ERRORLEVEL 1"
+::ossim-info --plugins | FINDSTR /C:%SUBSTRING% >nul & IF ERRORLEVEL 1 (
+::     echo "ERROR: Plugins, kakadu, png, and sqlite are needed for base testing and were not found."
+::     exit 1
+::)
 
 if not exist %OSSIM_BATCH_TEST_EXPECTED%(
   echo "ERROR: No expected results were detected in <%OSSIM_BATCH_TEST_EXPECTED%>. Cannot continue.";
